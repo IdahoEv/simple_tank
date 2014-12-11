a145ee72a1dc0fc43aed1fd5e3b5b0e55e3672b3
Evan Dorn <evan@lrdesign.com>
2014-12-10 21:08:10 -0800

    fix player test

--- test/player_test.exs
+++ test/player_test.exs
@@ -8,8 +8,8 @@ defmodule PlayerFacts do
   facts "new" do
 
     fact "two sequential players should not have the same player_id" do
-      pl1 = Player.new('John', 'pid1', 'pid2')
-      pl2 = Player.new('John', 'pid1', 'pid2')
+      pl1 = Player.new('John', 'pid1')
+      pl2 = Player.new('John', 'pid1')
       pl1.player_id |> !equals pl2.player_id
     end    
   end
