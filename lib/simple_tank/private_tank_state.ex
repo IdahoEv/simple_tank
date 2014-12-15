c3497ba21493450ddfa68647b35a6a6d16642f47
Evan Dorn <evan@lrdesign.com>
2014-12-14 16:34:35 -0800

    Add forgotten private tank state modules

---
+++ lib/simple_tank/private_tank_state.ex
@@ -0,0 +1,16 @@
+defmodule SimpleTank.PrivateTankState do
+
+  alias SimpleTank.Tank
+
+  def for_tank(player) do
+    tank_state = SimpleTank.Tank.get_state(player.tank_pid)    
+    %{ id: player.public_id, 
+       name:      player.name,
+       position:  tank_state.physics.position,
+       speed:     tank_state.physics.speed,
+       rotation:  tank_state.physics.rotation,
+       angular_velocity: tank_state.physics.angular_velocity             
+    }
+  end
+end
+
---
+++ test/private_tank_state_test.exs
@@ -0,0 +1,55 @@
+
+Code.require_file "../test_helper.exs", __ENV__.file
+
+defmodule PrivateTankStateFacts do
+  use Amrita.Sweet
+
+  alias SimpleTank.PrivateTankState
+  alias SimpleTank.TankPhysics
+  alias SimpleTank.Tank
+  alias SimpleTank.Player
+
+  def fixture_player(nn) do
+    player = %Player{
+      player_id:     "player_id_#{nn}",  
+      public_id:     "public_id_#{nn}",
+      name:          "player_name_#{nn}", 
+      tank_pid:      "tank_pid_#{nn}",
+      websocket_pid: "websocket_pid_#{nn}"
+    }
+  end
+  def fixture_tank(nn) do
+    tphys = %TankPhysics{
+          position: %{ x: (nn + 0.1), y: (nn + 0.2) },
+          rotation: nn + 0.3,
+          speed: nn + 0.4,
+          angular_velocity: nn + 0.5
+    }
+    tank = %Tank{ physics: tphys }
+  end 
+
+  fact "private tank state" do
+
+    provided [ SimpleTank.Tank.get_state(_) |> fixture_tank(1) ] do
+      state = PrivateTankState.for_tank(fixture_player(1))
+      state |> !equals nil 
+
+      # things the state should have
+      state.id        |> equals "public_id_1"
+      state.name      |> equals "player_name_1"
+      state.position  |> equals %{ x: 1.1, y: 1.2 }
+      state.rotation  |> equals 1.3
+      state.speed     |> equals 1.4
+      state.angular_velocity |> equals 1.5
+      
+      # things the public state shouldn't have
+      Dict.get(state, :public_id) |> equals nil  # is renamed just 'id' for API purposes
+      Dict.get(state, :velocity)  |> equals nil
+      Dict.get(state, :tank_pid)  |> equals nil
+      Dict.get(state, :game_pid)  |> equals nil
+      Dict.get(state, :websocket_pid)  |> equals nil
+    end 
+
+  end
+
+end
