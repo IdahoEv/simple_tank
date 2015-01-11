25f290436f187c2065513d3fe268449d053dbc7a
Evan Dorn <evan@lrdesign.com>
2015-01-10 21:11:22 -0800

    Move game updating out to a separate module and refactor into shorter methods.
    [#84524440]

--- lib/simple_tank/game_update.ex
+++ lib/simple_tank/game_update.ex
@@ -1,39 +1,55 @@
 defmodule SimpleTank.GameUpdate do
   alias SimpleTank.Game
+  alias SimpleTank.TankPhysics
+  alias SimpleTank.Bullet
+  alias SimpleTank.Tank
 
   def update(old_state) do
     # TODO: compute elapsed time, use a single delta for all upates
     #IO.puts("Updating world") 
-    alias SimpleTank.TankPhysics
-    alias SimpleTank.Bullet
-    alias SimpleTank.Tank
 
     bullet_list = SimpleTank.BulletList.update(old_state.bullet_list) 
+    tank_map = build_tank_map(old_state) 
 
     # make lists of bullet & tank geometries
-    bullet_geometry_list = Enum.map(bullet_list, 
-      fn(bullet) -> { bullet, Bullet.geometry(bullet)}
-    end)
-    tank_geometry_list = Enum.map(Dict.values(old_state.players), 
-      fn(player) -> { player, TankPhysics.geometry(Tank.get_state(player.tank_pid).physics) }
-    end)
+    bullet_geometry_list = get_bullet_geometries(bullet_list) 
+    tank_geometry_list = get_tank_geometries(tank_map)
 
-    # check for collisions
+    impacted_bullets = do_bullet_collisions(bullet_geometry_list, tank_geometry_list)
+
+    %Game{ old_state | bullet_list: (bullet_list -- impacted_bullets)}
+  end
+
+  def build_tank_map(old_state) do
+    Enum.map(Dict.values(old_state.players), 
+      fn(player) -> 
+        { player, Tank.get_state(player.tank_pid) }
+      end
+    )
+  end
+
+  def get_bullet_geometries(bullet_list) do
+    Enum.map(bullet_list, 
+    fn(bullet) -> { bullet, Bullet.geometry(bullet)}
+    end)
+  end
+
+  def get_tank_geometries(tank_map) do
+    Enum.map(tank_map, 
+    fn({player, tank}) -> { player, TankPhysics.geometry(tank.physics) }
+    end)
+  end
+
+  def do_bullet_collisions(bullet_geometry_list, tank_geometry_list) do
     collisions = Collider.ListCollider.find_hits(bullet_geometry_list, tank_geometry_list)
-    hit_bullets = []
-    hit_bullets = Enum.filter_map(collisions, 
+    Enum.filter_map(collisions, 
       fn({bullet, player, _}) -> 
-        if bullet.player_id != player.id do
-          IO.puts "Hit on #{player.name}!"            
-        end
         bullet.player_id != player.id
       end,
       fn({bullet, player, _}) -> 
+        IO.puts "Hit on #{player.name}!"            
         bullet
       end
     )    
-    # TODO: tell every tank to update? or let tanks self-update?     
-    %Game{ old_state | bullet_list: (bullet_list -- hit_bullets)}
   end
-  
 end
