96cfc80b9b8b316386bf6a52b334a37b1d76c0f0
Evan Dorn <evan@lrdesign.com>
2015-01-15 21:22:11 -0800

    define tank_hit event and handler

---
+++ test/simple_tank/tank_event_handler_test.exs
@@ -0,0 +1,21 @@
+
+Code.require_file "../../test_helper.exs", __ENV__.file
+
+defmodule TankEventHandlerFacts do
+  use Amrita.Sweet
+  alias SimpleTank.TankEventHandler
+
+  def fixture_pid, do:   make_ref
+  def fixture_id(n), do: "00000#{n}"
+
+  fact "for tank list" do
+    tank_pid = fixture_pid
+
+    provided [ SimpleTank.Tank.receive_hit(tank_pid) |> :ok ] do
+      TankEventHandler.handle_event({ :tank_hit, '1', '2', tank_pid}, []) |>
+        { :ok, [] }        
+    end
+  end
+  
+end
+
